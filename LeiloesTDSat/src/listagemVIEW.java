13172a6f6581a81f8e7a69edcda6bf67942aea52
Claudio Cesar <claudiocesardapaz@gmail.com>
2024-12-13 16:11:41 -0300

    Implementado o botão de vender produtos

--- LeiloesTDSat/src/listagemVIEW.java
+++ LeiloesTDSat/src/listagemVIEW.java
@@ -136,6 +136,7 @@ public class listagemVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
+        //botão vender produto
         String id = id_produto_venda.getText();
         
         ProdutosDAO produtosdao = new ProdutosDAO();
